8edff765cd551204fec225cca9f8c57dd9c1bbee
BiancaDias <bianca.dias@ufv.br>
2023-02-26 13:09:04 -0300

    correção para HUB

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -5,7 +5,7 @@ export default function Jogo(props){
             <img data-test="game-image" alt="imagem-forca" src={props.imagem}/>
             <div className="iniciaJogo">
                 <button data-test="choose-word" onClick={props.iniciaJogo} disabled={props.habilitado}> Escolher Palavra</button>
-                <div className={`palavra ${props.finalizandoOJogo}`} data-test="word">{props.palavra}</div>
+                <div data-test="word" className={`palavra ${props.finalizandoOJogo}`}>{props.palavra}</div>
             </div>
         </div>
         
